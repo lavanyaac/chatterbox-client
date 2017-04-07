48166ab58a5be97af0bf6262a5309e2d8bb9afb2
Hack Reactor Student <student@hackreactor.com>
2017-04-07 15:26:18 -0700

    Started building app.js

--- client/env/config.example.js
+++ client/env/config.example.js
@@ -6,3 +6,8 @@ $.ajaxPrefilter(function (settings, _, jqXHR) {
   jqXHR.setRequestHeader('X-Parse-Application-Id', 'PARSE_APP_ID');
   jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'PARSE_API_KEY');
 });
+
+
+// Server: http://parse.sfm8.hackreactor.com/
+// App ID: 72b8e073a4abde10221ce95f38ed1c63bd7f3d6b
+// API Key: cf1ce23a61e2a40702c347b7dc1e0af8c28f6c7a
--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,47 @@
 // YOUR CODE HERE:
+var app = {
+	
+	init: function(){},
+	send: function(message){
+		$.ajax({
+		  // This is the url you should use to communicate with the parse API server.
+		  url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+		  type: 'POST',
+		  data: JSON.stringify(message),
+		  contentType: 'application/json',
+		  success: function (data) {
+		    console.log('chatterbox: Message sent');
+		  },
+		  error: function (data) {
+		    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+		    console.error('chatterbox: Failed to send message', data);
+		  }
+		});
+	},
+	fetch: function(){
+		$.ajax({
+		  // This is the url you should use to communicate with the parse API server.
+		  // url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+		  type: 'GET',
+		  // data: JSON.stringify(message),
+		  // contentType: 'application/json',
+		  success: function (data) {
+		    console.log('chatterbox: Message received');
+		  },
+		  error: function (data) {
+		    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+		    console.error('chatterbox: Failed to fetch message', data);
+		  }
+		});
+	},
+	clearMessages: function (){},
+	renderMessage: function (){},
+	renderRoom: function (){}
+
+};
+
+// var message = {
+//   username: 'shawndrost',
+//   text: 'trololo',
+//   roomname: '4chan'
+// };
