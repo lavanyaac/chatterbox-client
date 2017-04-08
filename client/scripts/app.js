92dada5f1a9950b63fcf52a6448d711ee8d82113
Hack Reactor Student <student@hackreactor.com>
2017-04-08 11:16:08 -0700

    Completed Bare Minimum

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,15 +7,22 @@ $(document).ready(function (){
   });      
 
   $("#roomSelect").change(function(){
-    $("#roomSelect").find(':selected').val();
+    event.preventDefault();    
+    var dropDownVal = $("#roomSelect").find(':selected').val();
+    console.log('dropdownVal', dropDownVal)
+    if(dropDownVal === "add new room"){
+      console.log('went in')
+      var newRoom = (prompt('Enter new room name'))
+      app.renderRoom(newRoom);
+    }
     app.fetch();
   });
 
-  $("#chats").on('click', function(event){
+  $("#chats").on('click', '.username', function(event){
     event.preventDefault();
-    console.log('went in here');
-    app.handleUsernameClick();  
-  });      
+    var eventData = $(event.target).html();
+    app.handleUsernameClick(eventData);  
+  });   
 });
 
 
@@ -24,6 +31,7 @@ var app = {
   init: function(){
     this.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
     this.fetch();
+    this.friends = [];
   },
   send: function(message){
     $.ajax({
@@ -50,10 +58,8 @@ var app = {
       // This is the url you should use to communicate with the parse API server.
       url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
       type: 'GET',
-
       data: {limit: 1000, order: '-createdAt'},
       contentType: 'application/json',
-      // dataType: 'json',
 
       success: function (data) {       
         var results = data.results;
@@ -83,32 +89,48 @@ var app = {
   renderMessage: function (message){
     var $chatContainer = $('#chats')
     var $chat = $('<div>').addClass('chat').appendTo($chatContainer)
-    $('<div>').appendTo($chat).text(message.username).addClass('username');
+    var $userName = $('<div>').appendTo($chat).html(message.username).addClass('username');
+    if(app.friends.includes(message.username)){
+      $userName.addClass('friend');
+    }
     $('<div>').appendTo($chat).text(message.text).addClass('text');
     $('<div>').appendTo($chat).text(message.roomname);
     $('<div>').appendTo($chat).text(message.createdAt);
   },
   renderRoom: function (roomName){
     var $roomSelect = $('#roomSelect');
-    var $newRoomDrop = $('<option>').val(roomName);
+    var $newRoomDrop = $('<option>').text(roomName).attr({'value': roomName});
     $newRoomDrop.appendTo($roomSelect);
+    $roomSelect.val(roomName);
     return $("#roomSelect").val();
   },
   handleSubmit: function (){
     var inputText = $('#send').find('#message').val();
-    console.log('inputText', inputText);
     var userName = window.location.search.split("=").slice(-1).toString();
+    
+    console.log('roomname', $("#roomSelect").val())
+
     var message = {'username': userName,
       'text': inputText,
-      'roomName': 'Lobby'
+      'roomname': $("#roomSelect").val()
     };
+
     console.log(message);
+
     app.send(message);
-    app.clearMessages();
+    $('#send').find('#message').val('');
+    // app.clearMessages();
     app.fetch();
+
   },
-  handleUsernameClick: function (){
+  handleUsernameClick: function (userName){
     console.log('clicked on username')
+    if(!app.friends.includes(userName)){
+      console.log('pushed');
+      app.friends.push(userName);
+    }
+    console.log(userName, app.friends);
+    this.fetch();
   }
 
 };
@@ -117,47 +139,5 @@ var app = {
 // app.init();
 // app.fetch();
 // setInterval(function(){app.init(); app.fetch()},3000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var message = {
-//   username: 'alan and lavanya',
-//   text: 'trololo FROM 3:30PM',
-//   roomname: '4chan'
-// };
-
-// var testArr = [{
-//     username: 'alan and lavanya',
-//     text: 'trololo FROM 3:30PM',
-//     roomname: 'room1'
-//   },{
-//     username: 'alan and lavanya',
-//     text: 'trololo FROM 3:30PM',
-//     roomname: 'room2'
-//   },{
-//     username: 'alan and lavanya',
-//     text: 'trololo FROM 3:30PM',
-//     roomname: 'room3'
-//   },{
-//     username: 'alan and lavanya',
-//     text: 'trololo FROM 3:30PM',
-//     roomname: 'room4'
-//   },{
-//     username: 'alan and lavanya',
-//     text: 'trololo FROM 3:30PM',
-//     roomname: 'room5'
-//   }];
-
-// _.filter(testArr,function(obj){return obj.roomname === 'room1'});
 
 
