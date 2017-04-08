b4a309f4a6af7be3d63721bda1584ad73ca038d4
Hack Reactor Student <student@hackreactor.com>
2017-04-07 17:31:34 -0700

    Populated messages on chatterbox

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,7 @@ var app = {
 	send: function(message){
 		$.ajax({
 		  // This is the url you should use to communicate with the parse API server.
-		  url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+		  url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
 		  type: 'POST',
 		  data: JSON.stringify(message),
 		  contentType: 'application/json',
@@ -18,14 +18,38 @@ var app = {
 		  }
 		});
 	},
-	fetch: function(){
+	fetch: function(roomName){
 		$.ajax({
 		  // This is the url you should use to communicate with the parse API server.
-		  // url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+		  url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
 		  type: 'GET',
-		  // data: JSON.stringify(message),
-		  // contentType: 'application/json',
+		  dataType: 'json',
+		  // dataFilter: function (data){
+		  // 	// var returnVal = data;
+		  // 	console.log(JSON.parse(data));
+		  // 	return JSON.parse(data);},
 		  success: function (data) {
+        console.log(data);
+        //if roomname is defined
+          //do the filter thing and assign to results
+        var results = data.results;
+        if(roomName){
+          results = _.filter(data.results,function(obj){return obj.roomname === roomName});
+        }
+		  	for(var i of results){
+		  		var $chatContainer = $('#chats')
+		  		var $chat = $('<div>').addClass('chat').appendTo($chatContainer)
+		  		$('<div>').appendTo($chat).text(i.username).addClass('username');
+				  $('<div>').appendTo($chat).text(i.text).addClass('text');
+          $('<div>').appendTo($chat).text(i.roomname);
+		  	}
+
+        //if roomnamte is defined
+
+
+
+
+		  	// $('#chats').text(JSON.stringify(data));
 		    console.log('chatterbox: Message received');
 		  },
 		  error: function (data) {
@@ -34,14 +58,63 @@ var app = {
 		  }
 		});
 	},
-	clearMessages: function (){},
-	renderMessage: function (){},
+	clearMessages: function (){
+		var $chatContainer = $('#chats');
+    $chatContainer.html('');
+	},
+	renderMessage: function (message){},
 	renderRoom: function (){}
 
 };
 
+// setInterval(app.fetch,5000);
+
+function dropDownClick(){
+  var val = $('#dropdown').val();
+  console.log(val);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // var message = {
-//   username: 'shawndrost',
-//   text: 'trololo',
+//   username: 'alan and lavanya',
+//   text: 'trololo FROM 3:30PM',
 //   roomname: '4chan'
 // };
+
+// var testArr = [{
+//     username: 'alan and lavanya',
+//     text: 'trololo FROM 3:30PM',
+//     roomname: 'room1'
+//   },{
+//     username: 'alan and lavanya',
+//     text: 'trololo FROM 3:30PM',
+//     roomname: 'room2'
+//   },{
+//     username: 'alan and lavanya',
+//     text: 'trololo FROM 3:30PM',
+//     roomname: 'room3'
+//   },{
+//     username: 'alan and lavanya',
+//     text: 'trololo FROM 3:30PM',
+//     roomname: 'room4'
+//   },{
+//     username: 'alan and lavanya',
+//     text: 'trololo FROM 3:30PM',
+//     roomname: 'room5'
+//   }];
+
+// _.filter(testArr,function(obj){return obj.roomname === 'room1'});
+
+
