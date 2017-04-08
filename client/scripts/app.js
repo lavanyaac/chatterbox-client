d27e803c8738c84602fe02fc4e890e72f57a5c12
Hack Reactor Student <student@hackreactor.com>
2017-04-07 20:14:32 -0700

    Added drop for rooms, added submit feature

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,79 +1,92 @@
 // YOUR CODE HERE:
 var app = {
-	
-	init: function(){},
-	send: function(message){
-		$.ajax({
-		  // This is the url you should use to communicate with the parse API server.
-		  url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-		  type: 'POST',
-		  data: JSON.stringify(message),
-		  contentType: 'application/json',
-		  success: function (data) {
-		    console.log('chatterbox: Message sent');
-		  },
-		  error: function (data) {
-		    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-		    console.error('chatterbox: Failed to send message', data);
-		  }
-		});
-	},
-	fetch: function(roomName){
-		$.ajax({
-		  // This is the url you should use to communicate with the parse API server.
-		  url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-		  type: 'GET',
-		  dataType: 'json',
-		  // dataFilter: function (data){
-		  // 	// var returnVal = data;
-		  // 	console.log(JSON.parse(data));
-		  // 	return JSON.parse(data);},
-		  success: function (data) {
-        console.log(data);
-        //if roomname is defined
-          //do the filter thing and assign to results
+  
+  init: function(){
+    this.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
+    $("#dropdown").change(function(){
+        $("#dropdown").find(':selected').val();
+        app.fetch();
+    });
+
+    $("#messageInput").on('submit', function(event){
+      console.log('hi')
+        event.preventDefault();
+        var inputText = $("input[name='messageInput']").val();
+        var message = {'username': window.location.search.split('=')[1],
+          'text': inputText,
+          'roomName': app.renderRoom()
+        };
+        console.log('this works')
+        app.send(message);
+        // app.clearMessages();
+        app.fetch();
+    });  
+  },
+  send: function(message){
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  },
+  fetch: function(){
+    var roomName = this.renderRoom();
+
+    var that = this;
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+      type: 'GET',
+      dataType: 'json',
+
+      success: function (data) {       
         var results = data.results;
-        if(roomName){
+        
+        if (roomName === 'All') {
+          that.clearMessages();            
+        } else if(roomName){
+          that.clearMessages();
           results = _.filter(data.results,function(obj){return obj.roomname === roomName});
         }
-		  	for(var i of results){
-		  		var $chatContainer = $('#chats')
-		  		var $chat = $('<div>').addClass('chat').appendTo($chatContainer)
-		  		$('<div>').appendTo($chat).text(i.username).addClass('username');
-				  $('<div>').appendTo($chat).text(i.text).addClass('text');
-          $('<div>').appendTo($chat).text(i.roomname);
-		  	}
 
-        //if roomnamte is defined
+        for(var i of results){
+          that.renderMessage(i);
+        }
 
-
-
-
-		  	// $('#chats').text(JSON.stringify(data));
-		    console.log('chatterbox: Message received');
-		  },
-		  error: function (data) {
-		    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-		    console.error('chatterbox: Failed to fetch message', data);
-		  }
-		});
-	},
-	clearMessages: function (){
-		var $chatContainer = $('#chats');
-    $chatContainer.html('');
-	},
-	renderMessage: function (message){},
-	renderRoom: function (){}
+        console.log('chatterbox: Message received');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to fetch message', data);
+      }
+    });
+  },
+  clearMessages: function (){
+    $('#chats').html('');
+  },
+  renderMessage: function (message){
+    var $chatContainer = $('#chats')
+    var $chat = $('<div>').addClass('chat').appendTo($chatContainer)
+    $('<div>').appendTo($chat).text(message.username).addClass('username');
+    $('<div>').appendTo($chat).text(message.text).addClass('text');
+    $('<div>').appendTo($chat).text(message.roomname);
+  },
+  renderRoom: function (){
+    return $("#dropdown").val();
+  }
 
 };
 
-// setInterval(app.fetch,5000);
-
-function dropDownClick(){
-  var val = $('#dropdown').val();
-  console.log(val);
-}
-
+setInterval(function(){return app.fetch()},5000);
 
 
 
